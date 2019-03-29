f0af9a3daf601df960de0b673bd5845df6b66b5c
nielinjie <nielinjie@gmail.com>
2019-03-29 08:47:14 +0800

    tree graph

--- src/component/TreeGraph.tsx
+++ src/component/TreeGraph.tsx
@@ -1,19 +1,36 @@
 
 
-import { Graph, Node, Edge, Layouts } from 'viser-graph-react'
+import { Graph, Node, Edge, Layouts, registerNode, registerEdge } from 'viser-graph-react'
 import { FC, useState } from 'react';
 import * as React from 'react';
 import { useFetch } from 'react-hooks-fetch';
+import '@antv/g6/build/plugin.edge.polyline';
 
 const graph = {
   type: 'tree',
-  width: 2000,
+  width: 1000,
   height: 800,
-  fitView: 'lc',
+  fitView: 'autoZoom',
   fitViewPadding: true,
   animate: true,
   minZoom: 0.2,
   maxZoom: 10,
+  onAfterchange: function (ev, graph) {
+    graph.getNodes().forEach(function (node) {
+      var label = node.getLabel();
+      var keyShape = node.getKeyShape();
+      var box = keyShape.getBBox();
+      var labelBox = label.getBBox();
+      var dx = (box.maxX - box.minX + labelBox.maxX - labelBox.minX) / 2 + 8;
+      var dy = 0;
+      label.translate(dx, dy);
+    });
+    graph.draw();
+  },
+  onClick: function(ev, graph) {
+    console.log('click', ev, graph);
+    console.log(ev.item.model.style.stroke)
+  },
 };
 
 
@@ -29,12 +46,30 @@ const nodeAttr = {
 };
 
 const edgeAttr = {
-
+  shape: 'VH'
 }
 
+registerNode('treeNode', {
+  anchor: [[0, 0.5], [0.5, 1]]
+});
+registerNode('treeNodeF', {
+  getStyle: (item) =>{return {
+    stroke: 'red',
+  }},
+
+});
+
+registerEdge('VH', {
+  getPath: function getPath(item) {
+    var points = item.getPoints();
+    var start = points[0];
+    var end = points[points.length - 1];
+    return [['M', start.x, start.y], ['L', start.x, end.y], ['L', end.x, end.y]];
+  }
+});
 export const TreeGraph: FC = () => {
   // const [data, setData] = useState({ roots: [{}] });
-  
+
   var layout = new Layouts.IndentedTree({
     direction: 'LR', // 方向（LR/RL/H）
     indent: 30, // 缩进量
@@ -42,16 +77,18 @@ export const TreeGraph: FC = () => {
       return 4; // 竖向间距
     },
   })
-  const { error, data} = useFetch('http://...');
-  if (error) return <span>Error:{error.message}</span>;
+  const { error, data } = useFetch('http://localhost:8080/treeNode');
+  if (error) {
+    return <span>Error:{error.message}</span>;
+  }
   if (!data) return null; // this is important
   // return <span>RemoteData:{data}</span>;
 
-  
+
 
   return (
     <div>
-      <Graph {...graph} data={data} layout={layout}>
+      <Graph {...graph} data={{ roots: [data] }} layout={layout}>
         <Node {...nodeAttr} />
         <Edge {...edgeAttr} />
       </Graph>
